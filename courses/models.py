d4b75ca4b0f67378685f63ddd8a3cd87ffaee071
koena ramalepe <koenaramalepe2@gmail.com>
2024-06-30 16:19:22 +0200

    Added course and lesson models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name = 'Lessons', on_delete = models.CASCADE)
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
